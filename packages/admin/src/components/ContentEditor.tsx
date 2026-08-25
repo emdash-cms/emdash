f47545ff305f3446a7291e2035ac4acb7aa3bfa3
khoinguyenpham04 <137921741+khoinguyenpham04@users.noreply.github.com>
2026-08-25 17:24:34 +0100

    fix(admin): unescape resize handle label

--- packages/admin/src/components/ContentEditor.tsx
+++ packages/admin/src/components/ContentEditor.tsx
@@ -966,7 +966,7 @@ function ContentEditorSettingsResizeHandle({ panelId }: { panelId: string }) {
 	return (
 		<Sidebar.ResizeHandle
 			role="separator"
-			aria-label={t\`Resize settings panel\`}
+			aria-label={t`Resize settings panel`}
 			aria-orientation="vertical"
 			aria-controls={panelId}
 			aria-valuemin={minWidth}
